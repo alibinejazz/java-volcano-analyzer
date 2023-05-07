95e833f318bf40ee8c151c17d3708c6dc1451c76
alibinejazz <ali.ejaz@xloopdigital.co>
2023-05-07 20:02:46 +0500

    3 tests

--- src/main/java/VolcanoAnalyzer.java
+++ src/main/java/VolcanoAnalyzer.java
@@ -3,8 +3,11 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.TypeFactory;
@@ -13,22 +16,36 @@ public class VolcanoAnalyzer {
     private List<Volcano> volcanos;
 
     public void loadVolcanoes(Optional<String> pathOpt) throws IOException, URISyntaxException {
-        try{
+        try {
             String path = pathOpt.orElse("volcano.json");
             URL url = this.getClass().getClassLoader().getResource(path);
             String jsonString = new String(Files.readAllBytes(Paths.get(url.toURI())));
             ObjectMapper objectMapper = new ObjectMapper();
             TypeFactory typeFactory = objectMapper.getTypeFactory();
-            volcanos = objectMapper.readValue(jsonString, typeFactory.constructCollectionType(List.class, Volcano.class));
-        } catch(Exception e){
-            throw(e);
+            volcanos = objectMapper.readValue(jsonString,
+                    typeFactory.constructCollectionType(List.class, Volcano.class));
+        } catch (Exception e) {
+            throw (e);
         }
     }
 
-    public Integer numbVolcanoes(){
+    public Integer numbVolcanoes() {
         return volcanos.size();
     }
 
-    //add methods here to meet the requirements in README.md
+    public List<Volcano> eruptedInEighties() {
+        return volcanos.stream()
+                .filter(n -> n.getYear() >= 1980 && n.getYear() <= 1989)
+                .collect(Collectors.toList());
+    }
+
+    public String[] highVEI() {
+        return volcanos.stream().filter(v -> v.getVEI() >= 6).map(Volcano::getName).toArray(String[]::new);
+    }
+
+    public Volcano mostDeadly() {
+        return volcanos.stream().max(Comparator.comparing(Volcano::getDEATHS)).orElseThrow(null);
+    }
+    // add methods here to meet the requirements in README.md
 
 }
